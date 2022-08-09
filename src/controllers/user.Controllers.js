1dce7c42840711c27eb4f0022167adf37110e7bb
FredAbod <fredrickbolutife@gmail.com>
2022-08-09 13:34:32 +0100

    Added to main entry and models1

--- src/controllers/user.Controllers.js
+++ src/controllers/user.Controllers.js
@@ -29,7 +29,7 @@ exports.getApplicants = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: "Internal Server Error",
+      message: error.message,
     });
   }
 };
