9cf2ac95a6a025100d272bd5b361811571715640
FredAbod <fredrickbolutife@gmail.com>
2022-09-04 05:00:18 +0100

    added an https port

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,44 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER_MAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+const mailOptions = {
+  from: "boluwatifefred@gmail.com",
+  to: "fredrickbolutife@gmail.com",
+  subject: "You are under pressure",
+  text: "DB has stopped working ooooo guy!!!",
+};
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("connected to db");
   } catch (error) {
-    console.log(error.message);
-    process.exit()
+    let count = 0;
+    setTimeout(connectDB, 3000);
+    console.log("MongoDB connection unsuccessful, retry after 3 seconds");
+    ++count;
+    if (count == 5) {
+      // await transporter.sendMail(mailOptions);
+      transporter.sendMail(mailOptions, (error, info) => {
+        res.status(200).json({ message: "email Page" });
+        console.log(
+          "mongodb not connecting and Email Sent to " + info.accepted
+        );
+      });
+    }
   }
 };
 
 module.exports = connectDB;
+// process.abort();
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const morgan = require("morgan");
 const connectDB = require("./database/db");
 const userRouter = require("./routes/user.routes");
 const logger = require("morgan");
-
+const http = require("http");
+const https = require("https");
+const path = require("path");
 const app = express();
+const fs = require("fs");
 
 connectDB();
 app.use(logger("dev"));
@@ -13,7 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'localhost-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'localhost.pem')),
+};
 const port = process.env.PORT || 2300;
+const port2 = process.env.HTTPS_PORT || 300;
 
 app.get("/", (req, res) => res.send("Home Page"));
 
@@ -24,4 +32,7 @@ app.all("*", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server up and running on port http://localhost:${port}`);
+});
+https.createServer(options, app).listen(port2, () => {
+  console.log(`HTTP/2 listening on port https://localhost:${port2}`);
 });
